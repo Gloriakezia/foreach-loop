a3fd99c25acd876654fa7391a48953c4145196ac
Gloria Kezia <91767970+Gloriakezia@users.noreply.github.com>
2022-03-19 09:27:38 +0900

    push it

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+//loop
+
+//While Loop
+//Do while Loop
+//For Loop
+//Foreach Loop
+$array = array("Daniel", "Jane", "Jacob", "John", "Marine");
+
+foreach ($array as $loopdata) {
+    echo "My name is".$loopdata."<br>";
+}
+
+
+
+?>
